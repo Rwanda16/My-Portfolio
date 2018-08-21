466dcd3f1e2f0c8e1cc725884d33c81415511676
Gustave Habimana <ghabiman@gmail.com>
2018-08-20 22:45:59 -0400

    My Portfolio

---
+++ script.js/scrollReveal.js
@@ -0,0 +1,71 @@
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+    duration: 2000,
+    origin: 'bottom'
+});
+sr.reveal('#gustave', {
+    duration: 2000,
+    origin: 'top',
+    delay: 1000,
+    distance: '100px'
+});
+sr.reveal('.image-left', {
+    duration: 2000,
+    origin: 'top',
+    distance: '100px'
+});
+sr.reveal('.about-me', {
+    duration: 2000,
+    origin: 'right',
+    distance: '100px'
+});
+sr.reveal('#responsive i ', {
+    duration: 2000,
+    origin: 'left',
+    distance: '100px'
+});
+sr.reveal('#fast i', {
+    duration: 2000,
+    origin: 'left',
+    distance: '100px'
+});
+sr.reveal('#update i', {
+    duration: 2000,
+    origin: 'left',
+    distance: '100px'
+});
+sr.reveal('#handcrafted i', {
+    duration: 2000,
+    origin: 'left',
+    distance: '100px'
+});
+sr.reveal('#responsive h6 ', {
+    duration: 2000,
+    origin: 'top',
+    delay: 2000,
+    distance: '100px'
+});
+sr.reveal('#fast h6', {
+    duration: 2000,
+    origin: 'top',
+    delay: 2000,
+    distance: '100px'
+});
+sr.reveal('#update h6', {
+    duration: 2000,
+    origin: 'top',
+    delay: 2000,
+    distance: '100px'
+});
+sr.reveal('#handcrafted h6', {
+    duration: 2000,
+    origin: 'top',
+    delay: 2000,
+    distance: '100px'
+});
+sr.reveal('#dice-game', {
+    duration: 2000,
+    origin: 'top',
+    distance: '100px'
+});
+
